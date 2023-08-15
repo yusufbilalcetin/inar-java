75ddb88846df0aa57bca66dd77477b63e7b33744
Yusuf <yusufbilalctn@gmail.com>
2023-08-15 21:21:36 +0300

    updated

--- src/week_02/assignment/Question_02_17.java
+++ src/week_02/assignment/Question_02_17.java
@@ -5,16 +5,18 @@ import java.util.Scanner;
 public class Question_02_17 {
     public static void main(String[] args){
         Scanner input =new Scanner(System.in);
-        System.out.println("Enter the temperature in Fahrenheit between -58ºF and 41ºF :");
-        double outsideTemp = input.nextDouble();
-        double wc = 35.74 + 0.6215 - Math.pow(35.75 , 0.16) + Math.pow(0.4275 , 0.16);
 
-        
+        System.out.println("Enter the temperature in Fahrenheit between -58ºF and 41ºF:");
 
-        System.out.println("Enter the wind speed (>=2) in miles per hour : ");
-        double windSpeed = input.nextDouble();
-        double chill = Math.pow(wc , windSpeed);
-        System.out.println("The windSpeed chill index is " + chill);
+        double temperature = input.nextDouble();
+
+        System.out.println("Enter the wind speed (>=2) in miles per hour:");
+
+        double wind = input.nextDouble();
+
+        double widndChill = 35.74 + 0.6215 * temperature - 35.75 * Math.pow(wind, 0.16) + 0.4275 * temperature * Math.pow(wind, 0.16);
+
+        System.out.println("The wind chill index is " + (int)(widndChill * 10000) / 10000.0);
 
 
     }
