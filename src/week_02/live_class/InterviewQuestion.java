831027b1cb14c3d2074b438c2b9112a984997db4
Yusuf <yusufbilalctn@gmail.com>
2023-08-16 19:22:07 +0300

    test

--- src/week_02/live_class/InterviewQuestion.java
+++ src/week_02/live_class/InterviewQuestion.java
@@ -2,6 +2,7 @@ package week_02.live_class;
 
 public class InterviewQuestion {
     public static void main(String[] args) {
+        //We swap the values of x and y
         int x = 5;
         int y = 7;
 
@@ -9,7 +10,9 @@ public class InterviewQuestion {
         y = y - x;
 
         x = y + x;
-        System.out.println(x + " " + y);
+
+        System.out.println("x of value is " + x );
+        System.out.println("y of value is " + y );
 
 
 
