3bd40cc22213f9d24a79c37270ad2f3d4fc1aef4
Yusuf <yusufbilalctn@gmail.com>
2023-08-20 22:47:40 +0300

    updated

--- src/week_03/assignment/Question_03_02.java
+++ src/week_03/assignment/Question_03_02.java
@@ -1,7 +1,25 @@
 package week_03.assignment;
 
+import java.util.Scanner;
+
 public class Question_03_02 {
     public static void main(String[] args) {
+        int number1 = (int) (Math.random() * 10);
+        int number2 = (int) (Math.random() * 10);
+        int number3 = (int) (Math.random() * 10);
+
+
+            System.out.println("what is " + number1 + " + " + number2 + " + " + number3 + " ? ");
+            Scanner input = new Scanner(System.in);
+            int answer = input.nextInt();
+
+            if(number1 + number2 + number3 == answer) {
+                System.out.println(number1 + " + " + number2 + " +  " + number3 + " = " + answer + " is true");
+            } else
+                System.out.println(number1 + " + " + number2 + " + " + number3 + " = " + answer + " is false");
+            }
+
+
 
     }
-}
+
