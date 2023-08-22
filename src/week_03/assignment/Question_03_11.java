ab03d532f6b77829c0fcfe6a9ae15a302c3a9937
Yusuf <yusufbilalctn@gmail.com>
2023-08-22 17:20:15 +0300

    updated

--- src/week_03/assignment/Question_03_11.java
+++ src/week_03/assignment/Question_03_11.java
@@ -15,47 +15,47 @@ public class Question_03_11 {
         switch (month) {
             case 1:
                 daysInMonths = 31;
-                System.out.println("January" + " " + year + " had " + daysInMonths + " days");
+                System.out.println("January" + year + " had " + daysInMonths + " days");
                 break;
             case 3:
                 daysInMonths = 31;
-                System.out.println("march" + " " + year + " had " + daysInMonths + " days");
+                System.out.println("march "  + year + " had " + daysInMonths + " days");
                 break;
             case 5:
                 daysInMonths = 31;
-                System.out.println("May" + " " + year + " had " + daysInMonths + " days");
+                System.out.println("May " + year + " had " + daysInMonths + " days");
                 break;
             case 7:
                 daysInMonths = 31;
-                System.out.println("July" + " " + year + " had " + daysInMonths + " days");
+                System.out.println("July " + year + " had " + daysInMonths + " days");
                 break;
             case 8:
                 daysInMonths = 31;
-                System.out.println("august" + " " + year + " had " + daysInMonths + " days");
+                System.out.println("august " + year + " had " + daysInMonths + " days");
                 break;
             case 10:
                 daysInMonths = 31;
-                System.out.println("september " + " " + year + " had " + daysInMonths + " days");
+                System.out.println("september " + year + " had " + daysInMonths + " days");
                 break;
             case 12:
                 daysInMonths = 31;
-                System.out.println("december" + " " + year + " had " + daysInMonths + " days");
+                System.out.println("december " + year + " had " + daysInMonths + " days");
                 break;
             case 4:
                 daysInMonths = 30;
-                System.out.println(month + " " + year + " had " + daysInMonths + " days");
+                System.out.println("april " + year + " had " + daysInMonths + " days");
                 break;
             case 6:
                 daysInMonths = 30;
-                System.out.println(month + " " + year + " had " + daysInMonths + " days");
+                System.out.println("June " + year + " had " + daysInMonths + " days");
                 break;
             case 9:
                 daysInMonths = 30;
-                System.out.println(month + " " + year + " had " + daysInMonths + " days");
+                System.out.println("Semtember" + year + " had " + daysInMonths + " days");
                 break;
             case 11:
                 daysInMonths = 30;
-                System.out.println(month + " " + year + " had " + daysInMonths + " days");
+                System.out.println("November " + year + " had " + daysInMonths + " days");
                 break;
             case 2:
                 if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
