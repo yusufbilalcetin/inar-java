327d54255b2c4c5a69b2bad92e925eec310c7102
Yusuf <yusufbilalctn@gmail.com>
2023-08-22 19:42:21 +0300

    updated

--- src/week_03/assignment/Question_03_07.java
+++ src/week_03/assignment/Question_03_07.java
@@ -8,32 +8,45 @@ public class Question_03_07 {
         System.out.println("enter the amount (e.g 11,56): ");
         double amount = input.nextDouble();
 
-        if (amount < 0) {
-            System.out.println("invalid money");
-        } else if (amount == 0) {
-            System.out.println("0 $");
+        int remaningAmount = (int) (amount * 100);
+
+        int numberOfDollars = remaningAmount / 100;
+        remaningAmount = remaningAmount % 100;
+
+        int numberOfQuarters = remaningAmount / 25;
+        remaningAmount = remaningAmount % 25;
+
+        int numberOfDimes = remaningAmount / 10;
+        remaningAmount = remaningAmount % 10;
+
+        int numberOfNickels = remaningAmount / 5;
+        remaningAmount = remaningAmount % 5;
+
+        int numberOfPennies = remaningAmount;
+
+        System.out.println("your amount " + amount + " consists of ");
+
+        if (numberOfDollars > 1) {
+            System.out.println(numberOfDollars + " Dollars");
         } else {
-            int remaningAmount = (int)(amount * 100);
-
-            int numberOfDollars = remaningAmount / 100;
-            remaningAmount = remaningAmount % 100;
-
-            int numberOfQuarters = remaningAmount / 25;
-            remaningAmount = remaningAmount % 25;
-
-            int numberOfDimes = remaningAmount / 10;
-            remaningAmount = remaningAmount % 10;
-
-            int numberOfNickels = remaningAmount / 5;
-            remaningAmount = remaningAmount % 5;
-
-            int numberOfPennies = remaningAmount;
-            System.out.println("your amount " + amount + " consists of ");
-            System.out.println( numberOfDollars + " Dollars ");
-            System.out.println( numberOfQuarters + " Quarters ");
-            System.out.println( numberOfDimes + " Dimes ");
-            System.out.println( numberOfNickels + " Nickels ");
-            System.out.println( numberOfPennies + " pennies ");
+            System.out.println(numberOfDollars + " Dollar");
+        }
+        if (numberOfQuarters > 1) {
+            System.out.println(numberOfQuarters + " Quarters");
+        } else {
+            System.out.println(numberOfQuarters + " Quarter");
+        }if (numberOfDimes > 1){
+            System.out.println(numberOfDimes + " Dimes");
+        }else {
+            System.out.println(numberOfDimes + " Dime");
+        }if (numberOfNickels > 1) {
+            System.out.println(numberOfNickels + " Nickels");
+        }else {
+            System.out.println(numberOfNickels + " Nickel");
+        }if(numberOfPennies > 1){
+            System.out.println(numberOfPennies + " Pennies");
+        }else {
+            System.out.println(numberOfPennies + " Pennie");
         }
     }
 }
--- src/week_03/assignment/Question_03_13.java
+++ src/week_03/assignment/Question_03_13.java
@@ -19,15 +19,15 @@ public class Question_03_13 {
         if (status == 0) {
             if (income <= 8350)
                 tax = income * 0.10;
-        } else if (income <= 33950) {
+        } else if (income < 33950) {
             tax = 8350 * 0.10 + (income - 8350) * 0.15;
         } else if (income <= 82250) {
             tax = 8350 * 0.10 + (33950 - 8350) * 0.15 + (income - 33950) * 0.25;
-        } else if (income <= 171550) {
+        } else if (income < 171550) {
             tax = 8350 * 0.10 + (33950 - 8350) * 0.15 + (82250 - 33950) * 0.25 + (income - 82250) * 0.28;
         } else if (income <= 372950) {
             tax = 8350 * 0.10 + (33950 - 8350) * 0.15 + (82250 - 33950) * 0.25 + (171550 - 82250) * 0.28 + (income - 171550) * 0.33;
-        } else if (income <= 171550) {
+        } else if (income > 171550) {
             tax = 8350 * 0.10 + (33950 - 8350) * 0.15 + (82250 - 33950) * 0.25 + (171550 - 82250) * 0.28 + (372950 - 171550) * 0.33 + (income - 372950) * 0.35;
         }
         else if (status == 1) {
@@ -35,16 +35,16 @@ public class Question_03_13 {
                 tax = income * 0.10;
         } else if (income <= 67.900) {
             tax = 16700 * 0.10 + (income - 16700) * 15;
-        } else if (income <= 137.050) {
+        } else if (income <=137.050) {
             tax = 137050 * 0.10 + (137050 - 67900) * 15 + (income - 137.050) * 25;
-        } else if (income <= 208.850) {
+        } else if (income < 208.850) {
             tax = 137050 * 0.10 + (137050 - 67900) * 15 + (137050 - 67900) * 25 + (income - 137050) * 28;
-        } else if (income <= 372.950) {
+        } else if (income < 372.950) {
             tax = 137050 * 0.10 + (137050 - 67900) * 15 + (137050 - 67900) * 25 + (208.850 - 137050) * 28 + (income - 208850) * 33;
         } else if (income <= 372.950) {
             tax = 137050 * 0.10 + (137050 - 67900) * 15 + (137050 - 67900) * 25 + (208850 - 137050) * 28 + (372950 - 208850) * 33 + (income - 372.950) * 0.35;
         } else if (status == 2) {
-            if (income <= 8350)
+            if (income < 8350)
                 tax = income * 0.10;
         } else if (income <= 33950) {
             tax = 8350 * 0.10 + (income - 33950) * 15;
