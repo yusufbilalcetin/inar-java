af103004a98274bf50fb9d5f67085e54faea0518
Yusuf <yusufbilalctn@gmail.com>
2023-08-23 12:56:55 +0300

    updated

--- src/week_03/assignment/Question_03_03.java
+++ src/week_03/assignment/Question_03_03.java
@@ -5,7 +5,9 @@ import java.util.Scanner;
 public class Question_03_03 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
         System.out.println("Enter a, b, c, d, e, f: ");
+
         double a = input.nextDouble();
         double b = input.nextDouble();
         double c = input.nextDouble();
--- src/week_03/assignment/Question_03_14.java
+++ src/week_03/assignment/Question_03_14.java
@@ -1,7 +1,10 @@
 package week_03.assignment;
 
+import java.util.Scanner;
+
 public class Question_03_14 {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
     }
 }
