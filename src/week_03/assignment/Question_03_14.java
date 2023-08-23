f4f06a01153dd5528131daed3d9a61c8d0649648
Yusuf <yusufbilalctn@gmail.com>
2023-08-23 16:43:29 +0300

    updated

--- src/week_03/assignment/Question_03_14.java
+++ src/week_03/assignment/Question_03_14.java
@@ -6,5 +6,23 @@ public class Question_03_14 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
+        double random = Math.random();
+
+        System.out.println("Guess the flip of coin ( 0 represents head and 1 represents tail : ");
+        int answer = input.nextInt();
+
+        if ( random < 0.5 && answer < 1){
+            System.out.println("Your guess is correct");
+
+        }else if (random >= 0.5 && answer == 1) {
+            System.out.println("Your guess is correct");
+
+        }else if ( (random < 0.5 && answer == 1) || (random > 0.5 && answer < 1)){
+            System.out.println("Your guess is incorrect.");
+        }
+        else if (answer > 1 || answer < 0 ) {
+
+            System.out.println("Invalid value ");
+        }
     }
 }
