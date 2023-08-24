0de6799b5b32074f8063c1e551079c8517425e16
Yusuf <yusufbilalctn@gmail.com>
2023-08-24 15:35:19 +0300

    updated

--- src/week_03/assignment/Question_03_18.java
+++ src/week_03/assignment/Question_03_18.java
@@ -5,7 +5,23 @@ import java.util.Scanner;
 public class Question_03_18 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter the weight of the package (in pounds) : ");
+        System.out.print("Enter your package weight in pounds: ");
+        double weight = input.nextDouble();
+        double multiplier = 0;
 
+        if (weight > 0 && weight <= 1) {
+            multiplier = 3.5;
+        } else if (weight > 1 && weight <= 3) {
+            multiplier = 5.5;
+        } else if (weight > 3 && weight <= 10) {
+            multiplier = 8.5;
+        } else if (weight > 10 && weight <= 20) {
+            multiplier = 10.5;
+        }
+        if (multiplier == 0) {
+            System.out.println("this package cannot be shipped. ");
+        } else {
+            System.out.println("the shipping cost of package is $" + multiplier * weight);
+        }
     }
 }
---
+++ src/week_03/assignment/Question_03_19.java
@@ -0,0 +1,4 @@
+package week_03.assignment;
+
+public class Question_03_19 {
+}
