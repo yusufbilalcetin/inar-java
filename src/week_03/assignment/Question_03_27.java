7a02c54879a242af7be9b20981a79139e9b416a6
Yusuf <yusufbilalctn@gmail.com>
2023-08-25 18:16:23 +0300

    updated

--- src/week_03/assignment/Question_03_27.java
+++ src/week_03/assignment/Question_03_27.java
@@ -1,7 +1,18 @@
 package week_03.assignment;
 
+import java.util.Scanner;
+
 public class Question_03_27 {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a point's x- and y-coordinates: ");
+        double x = input.nextDouble();
+        double y = input.nextDouble();
 
+        if (100 * x + 200 * y <= 20000 && x >= 0 && x <= 200 && y >= 0 && y <= 100) {
+            System.out.println("The point is in the triangle");
+        } else {
+            System.out.println("The points is in the triangle");
+        }
     }
 }
