357a63a818cb9795da8faf6722737f873104e005
Yusuf <yusufbilalctn@gmail.com>
2023-08-28 11:47:00 +0300

    week_04

--- src/week_04/assignment/Question_04_01.java
+++ src/week_04/assignment/Question_04_01.java
@@ -1,4 +1,20 @@
 package week_04.assignment;
 
+import java.util.Scanner;
+
 public class Question_04_01 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter the length from the center to a vertex: ");
+        double r = input.nextDouble();
+
+        double s = 2 * r * Math.sin( Math.PI / 5 );
+        double area = 5 * Math.pow(s , 2) / (4 * Math.tan(Math.PI / 5));
+
+        System.out.printf("The are of the pentagon is %.2f" , area);
+
+
+
+    }
 }
