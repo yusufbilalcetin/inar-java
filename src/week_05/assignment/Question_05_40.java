a2689598663d0fadaeba2e487e6a2a879bbe04ea
Yusuf <yusufbilalctn@gmail.com>
2023-09-13 17:14:35 +0300

    week_05

--- src/week_05/assignment/Question_05_40.java
+++ src/week_05/assignment/Question_05_40.java
@@ -2,6 +2,20 @@ package week_05.assignment;
 
 public class Question_05_40 {
     public static void main(String[] args) {
-
+        int heads=0;
+        int tails=0;
+        int toss;
+        System.out.println("Flipping coins one million times...");
+        for (int i = 1; i <=1000000 ; i++) {
+            toss = (int)(Math.random()*2);
+            if(toss == 0){
+                heads++;
+            }
+            else {
+                tails++;
+            }
+        }
+        System.out.println("Heads: " + heads);
+        System.out.println("Tails: " + tails);
     }
 }
