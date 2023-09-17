de2e326c5c748e3c0cc33fb600147a8267de1b21
Yusuf <yusufbilalctn@gmail.com>
2023-09-17 17:47:04 +0300

    week_06

--- src/week_06/assignment/Question_06_13.java
+++ src/week_06/assignment/Question_06_13.java
@@ -2,11 +2,24 @@ package week_06.assignment;
 
 public class Question_06_13 {
     public static void main(String[] args) {
-        System.out.println("i         m(i)    \n " +
-                "--------------------------------------");
+        System.out.println("i        m(i)    \n" +
+                "-------------------------------");
+
+        double seriesStart = 1.0;
+        double numberOfRows = 20.0;
+
+        for (double i = seriesStart; i <= numberOfRows; i++) {
+            System.out.printf("%-10.0f", i);
+            System.out.printf("%7.4f\n", sumSeries(i));
+        }
     }
 
-    public static void sumSeries(int number){
-
+    public static double sumSeries(double n) {
+        double sum = 0;
+        for (double i = 1; i <= n; i++) {
+            sum += i / (i + 1);
+        }
+        return sum;
     }
 }
+
