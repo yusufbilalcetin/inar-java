c6d72a388f32611483c352b22c510714dcb4ef90
Yusuf <yusufbilalctn@gmail.com>
2023-09-17 18:26:11 +0300

    week_06

--- src/week_06/assignment/Question_06_15.java
+++ src/week_06/assignment/Question_06_15.java
@@ -2,6 +2,12 @@ package week_06.assignment;
 
 public class Question_06_15 {
     public static void main(String[] args) {
+        System.out.println("Taxable Income     Single     Married joint or Quealifying widow(er)     Married Separate     Head of a house"
+        + "\n---------------------------------------------------------------------------------------------------------------");
+
+    }
+
+    public static  double computeTax(double status, double taxableIncome){
 
     }
 }
