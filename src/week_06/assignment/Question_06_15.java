e0a04a06db89d9d68eafbab228204000dcaa9829
Yusuf <yusufbilalctn@gmail.com>
2023-09-18 16:42:37 +0300

    week_06

--- src/week_06/assignment/Question_06_15.java
+++ src/week_06/assignment/Question_06_15.java
@@ -2,17 +2,86 @@ package week_06.assignment;
 
 public class Question_06_15 {
     public static void main(String[] args) {
-        System.out.println("Taxable Income     Single     Married joint or Quealifying widow(er)     Married Separate     Head of a house"
-                + "\n---------------------------------------------------------------------------------------------------------------");
-        int status = 50_000;
-        int taxableIncome = 60_000;
-        computeTax(status, taxableIncome);
-    }
+        System.out.println("\nTaxable      Single      Married Joint       Married        Head of\n" +
+                "Income                   or Qualifying       Separate       a House\n" +
+                "                         Widow(er)\n" +
+                "---------------------------------------------------------------------");
+        double from = 50000;
+        double to = 60000;
+        double interval = 50;
 
-    public static void computeTax(double status, double taxableIncome) {
-        for (int i = 50_000; i <= 60_000; i += 50) {
-            System.out.printf("%5d", status, taxableIncome);
-
+        for (double taxableIncome = from;
+             taxableIncome <= to; taxableIncome += interval) {
+            System.out.printf("%-13.0f", taxableIncome);
+            System.out.printf("%-12d", Math.round(computeTax(0, taxableIncome)));
+            System.out.printf("%-20d", Math.round(computeTax(1, taxableIncome)));
+            System.out.printf("%-17d", Math.round(computeTax(2, taxableIncome)));
+            System.out.printf("%-15d\n", Math.round(computeTax(3, taxableIncome)));
         }
     }
+
+    public static double computeTax(int status, double taxableIncome) {
+        double taxAt15Pecent = 0.0;
+        double taxAt25Pecent = 0.0;
+        double taxAt28Pecent = 0.0;
+        double taxAt33Pecent = 0.0;
+        double taxAt35Pecent = 0.0;
+        double incomeUntaxed = 0.0;
+        double incomeTaxed;
+        double taxRate = 0.0;
+        double tax = 0.0;
+
+        switch (status) {
+            case 0://---------------------------{Single}---------------------------
+                taxAt15Pecent = 8351;
+                taxAt25Pecent = 33951;
+                taxAt28Pecent = 82251;
+                taxAt33Pecent = 171551;
+                taxAt35Pecent = 372951;
+                break;
+            case 1://-------------{Married Joint Or Qualifying Widow(er)}----------
+                taxAt15Pecent = 16701;
+                taxAt25Pecent = 67901;
+                taxAt28Pecent = 137051;
+                taxAt33Pecent = 208851;
+                taxAt35Pecent = 372951;
+                break;
+            case 2://----------------{Married Filling Separately}------------------
+                taxAt15Pecent = 8351;
+                taxAt25Pecent = 33951;
+                taxAt28Pecent = 68526;
+                taxAt33Pecent = 104426;
+                taxAt35Pecent = 186476;
+                break;
+            case 3://----------------------{Head Of house}-------------------------
+                taxAt15Pecent = 11951;
+                taxAt25Pecent = 45501;
+                taxAt28Pecent = 117451;
+                taxAt33Pecent = 190201;
+                taxAt35Pecent = 372951;
+                break;
+        }
+        while (taxableIncome >= taxAt15Pecent) {
+            if (taxableIncome >= taxAt35Pecent) {
+                taxRate = 0.35;
+                incomeUntaxed = taxAt35Pecent - 1;
+            } else if (taxableIncome >= taxAt33Pecent) {
+                taxRate = 0.33;
+                incomeUntaxed = taxAt33Pecent - 1;
+            } else if (taxableIncome >= taxAt28Pecent) {
+                taxRate = 0.28;
+                incomeUntaxed = taxAt28Pecent - 1;
+            } else if (taxableIncome >= taxAt25Pecent) {
+                taxRate = 0.25;
+                incomeUntaxed = taxAt25Pecent - 1;
+            } else if (taxableIncome >= taxAt15Pecent) {
+                taxRate = 0.15;
+                incomeUntaxed = taxAt15Pecent - 1;
+            }
+            tax += (incomeTaxed = taxableIncome - incomeUntaxed) * taxRate;
+            taxableIncome -= incomeTaxed;
+        }
+        return tax += taxableIncome * 0.10;
+    }
 }
+
