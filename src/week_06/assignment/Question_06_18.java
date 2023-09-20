71a5b79348dc17d957a70789f3a390f18c111fc4
Yusuf <yusufbilalctn@gmail.com>
2023-09-20 18:59:07 +0300

    week_06

--- src/week_06/assignment/Question_06_18.java
+++ src/week_06/assignment/Question_06_18.java
@@ -9,29 +9,7 @@ public class Question_06_18 {
         System.out.println("Enter a password: ");
         String password = input.nextLine();
 
-        // System.out.print(validPassword(password) ? " valid " : " invalid "
-        //   + " Password");
-        //  validPassword(password);
-    }
-}
-
-//public static boolean validPassword(boolean password) {
-//  final int CHARACTERS = 8;
-//int validPassword = isLenghtValid(password, CHARACTERS)
-//      && digitsAndLetters(password)
-//    && noDigits(password, 2);
-
-//return validPassword;
-
-
-//public static boolean isLenghtValid(String password, int number) {
-//  return password.length() >= number;
-
-
-//public static boolean digitsAndLetters(boolean password) {
-
-
-//public static boolean noDigits(boolean password, int number) {
+        
 
 
 
