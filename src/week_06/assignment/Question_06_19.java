69c5b2d189e91b4896808a2ccd9ac09a86d54875
Yusuf <yusufbilalctn@gmail.com>
2023-09-20 19:50:10 +0300

    week_06

--- src/week_06/assignment/Question_06_19.java
+++ src/week_06/assignment/Question_06_19.java
@@ -20,15 +20,7 @@ public class Question_06_19 {
 
 
     public static boolean isValid(double side1, double side2, double side3) {
-        if ((side1 + side2) > side3) {
-            return true;
-        } else if ((side1 + side3) > side2) {
-            return true;
-        } else if ((side2 + side3) > side1) {
-            return true;
-        } else {
-            return false;
-        }
+        
     }
 
     public static double area(double side1, double side2, double side3) {
