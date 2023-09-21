24abb419755a6b6e9f5b49a4327fb16c12d11490
Yusuf <yusufbilalctn@gmail.com>
2023-09-21 21:45:54 +0300

    week_06

--- src/week_06/assignment/Question_06_28.java
+++ src/week_06/assignment/Question_06_28.java
@@ -2,7 +2,7 @@ package week_06.assignment;
 
 public class Question_06_28 {
     public static void main(String[] args) {
-        System.out.printf("%15s%-15s\n", "p", "2^^p-1");
+        System.out.printf("%-15s%-15s\n", "p", "2^^p-1");
         System.out.println("-----------------------------");
         for (int i = 1; i <= 31; i++) {
             if (isMersennePrime(i)) {
