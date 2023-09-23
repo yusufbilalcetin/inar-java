78d02d0e5a0e74b5285a8ae726a0b867855764e2
Yusuf <yusufbilalctn@gmail.com>
2023-09-23 15:40:19 +0300

    week_06

--- src/week_06/assignment/Question_06_23.java
+++ src/week_06/assignment/Question_06_23.java
@@ -22,7 +22,7 @@ public class Question_06_23 {
     public static int count(String s, char ch) {
         int count = 0;
         for (int i = 0; i < s.length(); i++) {
-            if (ch == s.charAt(i)) {
+            if (s.toLowerCase().charAt(i) == Character.toLowerCase(ch)) {
                 count++;
             }
         }
--- src/week_06/assignment/Question_06_24.java
+++ src/week_06/assignment/Question_06_24.java
@@ -2,6 +2,6 @@ package week_06.assignment;
 
 public class Question_06_24 {
     public static void main(String[] args) {
-        
+
     }
 }
