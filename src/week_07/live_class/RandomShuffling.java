1f41b85331fc965d17894db26cfac06d877c8662
Yusuf <yusufbilalctn@gmail.com>
2023-09-29 16:18:24 +0300

    week_07

--- src/week_07/live_class/RandomShuffling.java
+++ src/week_07/live_class/RandomShuffling.java
@@ -2,6 +2,17 @@ package week_07.live_class;
 
 public class RandomShuffling {
     public static void main(String[] args) {
-        
+        int[] myList = new int[10];
+        for (int i = 0; i < myList.length; i++) {
+            myList[i] += i;
+        }
+
+        for (int i = 0; i < myList.length - 1; i++) {
+            int j = (int) (Math.random() * (i + 1));
+            int temp = myList[i];
+            myList[i] = myList[j];
+            myList[j] = temp;
+        }
+        System.out.println();
     }
 }
---
+++ src/week_07/live_class/ShiftingElements.java
@@ -0,0 +1,11 @@
+package week_07.live_class;
+
+public class ShiftingElements {
+    public static void main(String[] args) {
+        int[] myList = new int[10];
+        for (int i = 0; i < myList.length; i++) {
+            myList[i] += i;
+        }
+        
+    }
+}
