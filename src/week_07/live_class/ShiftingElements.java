1429d853f7364c1fa95e82a9881829ef32780bb1
Yusuf <yusufbilalctn@gmail.com>
2023-10-01 11:05:14 +0300

    week_07

--- src/week_07/live_class/ShiftingElements.java
+++ src/week_07/live_class/ShiftingElements.java
@@ -2,10 +2,18 @@ package week_07.live_class;
 
 public class ShiftingElements {
     public static void main(String[] args) {
+
         int[] myList = new int[10];
+
         for (int i = 0; i < myList.length; i++) {
             myList[i] += i;
         }
-        
+        for (int i = 0; i < myList.length; i++) {
+            myList[i - 1] = myList[i];
+        }
+
+        int temp = 0;
+        myList[myList.length - 1] = temp;
+        System.out.println(temp);
     }
 }
