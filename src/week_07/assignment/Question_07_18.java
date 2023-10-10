a21750d7005a737ce7b70372f2cd5342a344f763
Yusuf <yusufbilalctn@gmail.com>
2023-10-10 16:58:40 +0300

    week_07

--- src/week_07/assignment/Question_07_18.java
+++ src/week_07/assignment/Question_07_18.java
@@ -1,7 +1,28 @@
 package week_07.assignment;
 
+import java.util.Scanner;
+
 public class Question_07_18 {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter ten numbers: ");
+        double[] array = new double[10];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = input.nextInt();
+        }
+        bubbleSort(array);
+        for (double e : array) {
+            System.out.println(e + " ");
+        }
+    }
 
+    public static void bubbleSort(double[] array) {
+        for (int i = 0 ; i < array.length; i++) {
+            if (array[i] > array[i + 1]) {
+                double temp = array[i];
+                array[i] = array[i + 1];
+                array[i] = temp;
+            }
+        }
     }
 }
