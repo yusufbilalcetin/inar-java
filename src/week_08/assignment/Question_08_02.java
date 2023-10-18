133586f7c9990d9300e11d644d0b62ff94141ecf
Yusuf Bilal <https://github.com/yusufbilalcetin/inar-java.git>
2023-10-18 14:01:03 +0200

    week_08

--- src/week_08/assignment/Question_08_02.java
+++ src/week_08/assignment/Question_08_02.java
@@ -6,4 +6,32 @@ public class Question_08_02 {
     public static void main(String[] args) {
         double[][] matrix = getArray(4, 4);
 
+            System.out.println("Sum of the elements in the major diagonal is " + sumMajorDiagonal(matrix));
+
+    }
+
+    private static double[][] getArray(int i, int j) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a 4-by-4 matrix row by row: ");
+        double[][] matrix = new double[i][j];
+
+        for (int y = 0; y < matrix.length; y++) {
+            for (int k = 0; k < matrix[y].length; k++) {
+                matrix[y][k] = input.nextDouble();
+            }
+        }
+        return matrix;
+    }
+
+    private static double sumMajorDiagonal(double[][] m) {
+        double sum = 0;
+        double sum2 = 0;
+        for (int i = 0; i < m.length; i++) {
+            sum += m[i][i];
+        }
+        for (int i = 0, l = m.length - 1; i < m.length; i++, l--) {
+            sum2 += m[i][l];
+        }
+        return Math.max(sum, sum2);
+    }
 }
---
+++ src/week_08/assignment/Question_08_03.java
@@ -0,0 +1,23 @@
+package week_08.assignment;
+
+public class Question_08_03 {
+    public static void main(String[] args) {
+        char[][] answers = {
+                {'A', 'B', 'A', 'C', 'C', 'D', 'E', 'E', 'A', 'D'},
+                {'D', 'B', 'A', 'B', 'C', 'A', 'E', 'E', 'A', 'D'},
+                {'E', 'D', 'D', 'A', 'C', 'B', 'E', 'E', 'A', 'D'},
+                {'C', 'B', 'A', 'E', 'D', 'C', 'E', 'E', 'A', 'D'},
+                {'A', 'B', 'D', 'C', 'C', 'D', 'E', 'E', 'A', 'D'},
+                {'B', 'B', 'E', 'C', 'C', 'D', 'E', 'E', 'A', 'D'},
+                {'B', 'B', 'A', 'C', 'C', 'D', 'E', 'E', 'A', 'D'},
+                {'E', 'B', 'E', 'C', 'C', 'D', 'E', 'E', 'A', 'D'}};
+
+
+        char[] keys = {'D', 'B', 'D', 'C', 'C', 'D', 'A', 'E', 'A', 'D'};
+
+        
+    }
+}
+
+
+
