7c4d3d67944effc27289d22d6d39f417abeec3f0
Yusuf Bilal <yusufbilalctn@gmail.com>
2023-10-26 15:38:33 +0200

    week_08

--- src/week_08/assignment/Question_08_24.java
+++ src/week_08/assignment/Question_08_24.java
@@ -1,7 +1,75 @@
 package week_08.assignment;
 
+import java.util.Scanner;
+
 public class Question_08_24 {
     public static void main(String[] args) {
+        /*
+        3 2 7 8 9 5 4 1 6
+        1 4 8 6 3 7 9 5 2
+        6 5 9 2 4 1 8 3 7
+        7 1 5 9 6 4 2 8 3
+        2 6 4 1 8 3 7 9 5
+        9 8 3 7 5 2 6 4 1
+        5 3 6 4 2 8 1 7 9
+        8 7 2 5 1 9 3 6 4
+        4 9 1 3 7 6 5 2 8
+------------------------------------
+        9 6 3 1 7 4 2 5 8
+        1 7 8 3 2 5 6 4 9
+        2 5 4 6 8 9 7 3 1
+        8 2 1 4 3 7 5 9 6
+        4 9 6 8 5 2 3 1 7
+        7 3 5 9 6 1 8 2 4
+        5 8 9 7 1 3 4 6 2
+        3 1 7 2 4 6 9 8 5
+        6 4 2 5 9 8 1 7 3
+         */
+        int[][] grid = readAsolution();
+        System.out.println(isValid(grid) ? "Valid solution" : "Invalid solution");
+    }
 
+    public static int[][] readAsolution() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a sudoku puzzle solution: ");
+        int[][] grid = new int[9][9];
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                grid[i][j] = input.nextInt();
+            }
+        }
+        return grid;
+    }
+
+    public static boolean isValid(int[][] grid) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (grid[i][j] < 1 || grid[i][j] > 9 || isValid(i, j, grid)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static boolean isValid(int i, int j, int[][] grid) {
+        for (int column = 0; column < 9; column++) {
+            if (column != j && grid[i][column] == grid[i][j]) {
+                return false;
+            }
+        }
+        for (int row = 0; row < 9; row++) {
+            if (row != i && grid[row][j] == grid[i][j]) {
+                return false;
+            }
+        }
+        for (int row = (i / 3) * 3; row < (i / 3) * 3 + 3; row++) {
+            for (int column = (j / 3) * 3; column < (j / 3) * 3 + 3; column++) {
+                if (row != i && column != j && grid[row][column] == grid[i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
--- src/week_08/assignment/Question_08_26.java
+++ src/week_08/assignment/Question_08_26.java
@@ -4,9 +4,19 @@ import java.util.Scanner;
 
 public class Question_08_26 {
     public static void main(String[] args) {
+        /*
+        0.15 0.875 0.375
+        0.55 0.005 0.225
+        0.30 0.12 0.4
+         */
         double[][] matrix = getMatrixFromUser();
+        displayMatrix(matrix);
 
-        System.out.println("The row-sorted array is \n" + sortRows(matrix));
+        double[][] matrixSorted = CopyMatrix(matrix);
+        sortRows(matrixSorted);
+
+        System.out.println("The row-sorted array is ");
+        displayMatrix(matrixSorted);
     }
 
     public static double[][] getMatrixFromUser() {
@@ -21,8 +31,42 @@ public class Question_08_26 {
         return matrix;
     }
 
+    private static double[][] CopyMatrix(double[][] matrix) {
+        double[][] sortedMatrix = new double[3][3];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                sortedMatrix[i][j] = matrix[i][j];
+            }
+        }
+        return sortedMatrix;
+    }
+
+    private static void displayMatrix(double[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
     public static double[][] sortRows(double[][] m) {
-
-
+        for (int row = 0; row < m.length; row++) {
+            for (int column = 0; column < m[row].length; column++) {
+                double min = m[row][column];
+                int minIndex = column;
+                for (int i = column + 1; i < m.length; i++) {
+                    if (m[row][i] < min) {
+                        min = m[row][i];
+                        minIndex = i;
+                    }
+                }
+                if (minIndex != column) {
+                    m[row][minIndex] = m[row][column];
+                    m[row][column] = min;
+                }
+            }
+        }
+        return m;
     }
 }
