100d6abb9053e86156af795eeef9a3e59e039373
Yusuf Bilal <yusufbilalctn@gmail.com>
2023-11-02 12:13:56 +0100

    week_09

--- src/week_09/assignment/StockTest_09_02.java
+++ src/week_09/assignment/StockTest_09_02.java
@@ -1,4 +1,12 @@
 package week_09.assignment;
 
 public class StockTest_09_02 {
+    public static void main(String[] args) {
+
+        Stock_09_02 stocked1 = new Stock_09_02("ORCL", "Oracle Corporation", 34.5, 34.35);
+
+        System.out.println("Stock name       : " + stocked1.getName());
+        System.out.println("Stock symbol     : " + stocked1.getSymbol());
+        System.out.println("Price-change percentage: " + stocked1.getChangePercent());
+    }
 }
--- src/week_09/assignment/Stock_09_02.java
+++ src/week_09/assignment/Stock_09_02.java
@@ -1,6 +1,44 @@
 package week_09.assignment;
 
 public class Stock_09_02 {
+    private final String symbol;
+    private final String name;
+    private double previousClosingPrice;
+    private double currentPrice;
 
+    public Stock_09_02(String symbol, String name, double previousClosingPrice, double currentPrice) {
+        this.symbol = symbol;
+        this.name = name;
+        this.previousClosingPrice = previousClosingPrice;
+        this.currentPrice = currentPrice;
+    }
+
+    public double getChangePercent() {
+        return ((currentPrice - previousClosingPrice) / previousClosingPrice) * 100;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPreviousClosingPrice() {
+        return previousClosingPrice;
+    }
+
+    public double getCurrentPrice() {
+        return currentPrice;
+    }
+
+    public void setCurrentPrice(double currentPrice) {
+        this.currentPrice = currentPrice;
+    }
+
+    public void setPreviousClosingPrice(double previousClosingPrice) {
+        this.previousClosingPrice = previousClosingPrice;
+    }
 
 }
