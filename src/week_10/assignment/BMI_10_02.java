6b8003736cd7556a1d2a0829672005e5c098620f
Yusuf Bilal <yusufbilalctn@gmail.com>
2023-11-07 13:15:12 +0100

    week_10

--- src/week_10/assignment/BMI_10_02.java
+++ src/week_10/assignment/BMI_10_02.java
@@ -4,18 +4,85 @@ public class BMI_10_02 {
     private String name;
     private int age;
     private double weight;
+    private double height;
     private double feet;
     private double inches;
+    public static final double KILOGRAMS_PER_POUNDS = 0.45359237;
+    public static final double METERS_PER_INCH = 0.0254;
 
-    public BMI_10_02() {
+    public BMI_10_02(String name, int age, double weight, double height) {
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+        this.height = height;
+    }
+
+    public BMI_10_02(String name, int weight, double height) {
+        this.name = name;
+        this.weight = weight;
+        this.height = height;
     }
 
     public BMI_10_02(String name, int age, double weight, double feet, double inches) {
         this.name = name;
         this.age = age;
         this.weight = weight;
-        this.feet = feet;
-        this.inches = inches;
+        this.height = (feet * 12) + inches;
     }
 
+    public double getBMI() {
+        double bmi = weight * KILOGRAMS_PER_POUNDS / ((height * METERS_PER_INCH) * (height * METERS_PER_INCH));
+        return Math.round(bmi * 100) / 100.0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public String getStatus() {
+        double bmi = getBMI();
+        if (bmi < 18.5) {
+            return "Underweight";
+        } else if (bmi < 25) {
+            return "Normal";
+        } else if (bmi < 30) {
+            return "Overweight";
+        } else {
+            return "Obese";
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Name : " + name + "\n" +
+                "BMI : " + getBMI() + " " + getStatus();
+    }
 }
---
+++ src/week_10/assignment/BmiTest_10_02.java
@@ -0,0 +1,10 @@
+package week_10.assignment;
+
+public class BmiTest_10_02 {
+    public static void main(String[] args) {
+        BMI_10_02 bmi1 = new BMI_10_02("Martin", 18, 168.50, 1, 160);
+        System.out.println(bmi1);
+        BMI_10_02 bmi2 = new BMI_10_02("adam smith", 75, 175);
+        System.out.println(bmi2);
+    }
+}
