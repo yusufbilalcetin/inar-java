bb80e6acef4986d1e91da6babe4ace3bd1a8db66
Yusuf Bilal <yusufbilalctn@gmail.com>
2023-11-15 18:02:39 +0100

    week_11

--- src/week_11/live_class/TestArrayList.java
+++ src/week_11/live_class/TestArrayList.java
@@ -18,7 +18,6 @@ public class TestArrayList {
         System.out.println("Is Miami in the list ? " + cityList.contains("Miami"));
         System.out.println("Is the list emtpy ? " + cityList.isEmpty());
 
-
         cityList.add(2, "Xian");
         cityList.remove("Miami");
         cityList.remove(1);
@@ -31,7 +30,6 @@ public class TestArrayList {
 
         list.add(new CircleFromSimpleGeometricObject(2));
         list.add(new CircleFromSimpleGeometricObject(3));
-
 
         System.out.println("The area of the circle ? " + list.get(0).getArea());
     }
