c8945e248a0b3d0b42dad9db27ae2b91b9059249
Yusuf Bilal <yusufbilalctn@gmail.com>
2023-11-22 17:24:03 +0100

    week_12

--- src/week_12/assignment/Question_12_04/IllegalArgumentException.java
+++ src/week_12/assignment/Question_12_04/IllegalArgumentException.java
@@ -4,8 +4,7 @@ public class IllegalArgumentException {
     public static void main(String[] args) {
         try {
             LoanWithException loan = new LoanWithException(3.5, 5, 0);
-        } catch (java.lang.IllegalArgumentException
-                ex) {
+        } catch (java.lang.IllegalArgumentException ex) {
             System.out.println("Wrong input! Please greater than 0 (zero)");
         }
     }
