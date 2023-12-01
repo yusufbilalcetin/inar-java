4ae59c96d9da7611d32c410d2c0658938d2a96f2
Yusuf Bilal <yusufbilalctn@gmail.com>
2023-12-01 15:19:06 +0100

    week_13

--- src/week_13/Question_13_13/Course.java
+++ src/week_13/Question_13_13/Course.java
@@ -1,28 +1,70 @@
 package week_13.Question_13_13;
 
-public class Course {
+public class Course implements Cloneable {
     private String courseName;
-    private String[] student = new String[100];
-    private int numberOfStudent;
+    private String[] students = new String[10];
+    private int numberOfStudents;
 
     public Course(String courseName) {
         this.courseName = courseName;
     }
 
-    public void addStudent(String student) {
-        student[numberOfStudent] = student;
-        numberOfStudent++;
+    public void addStudents(String student) {
+        if (numberOfStudents >= students.length) {
+            String[] tempStudent = new String[students.length * 2];
+            System.arraycopy(students, 0, tempStudent, 0, students.length);
+            students = tempStudent;
+        }
+        students[numberOfStudents] = student;
+        numberOfStudents++;
     }
 
-    public String[] getStudent() {
-        return student;
+    public void dropStudent(String student) {
+        int index = findTheSpecifiedStudent(student);
+        if (index == -1) {
+            System.out.printf("There aren't any student named %s in this course.\n", student);
+            return;
+        }
+        for (int i = index; i < students.length - 1; i++) {
+            students[i] = students[i + 1];
+        }
+        numberOfStudents--;
     }
 
-    public int getNumberOfStudent() {
-        return numberOfStudent;
+    public int findTheSpecifiedStudent(String student) {
+        for (int i = 0; i < students.length; i++) {
+            if (student.equals((students[i]))) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public void clear() {
+        students = new String[10];
+        numberOfStudents = 0;
     }
 
     public String getCourseName() {
+        return courseName;
+    }
+
+    public String[] getStudents() {
+        return students;
+    }
+
+    public int getNumberOfStudents() {
+        return numberOfStudents;
+    }
+
+    @Override
+    public Course clone() throws CloneNotSupportedException {
+        Course courseClone = (Course) super.clone();
+        courseClone.students = (String[]) students.clone();
+        return courseClone;
+    }
+
+    public String toString() {
         return courseName;
     }
 }
---
+++ src/week_13/Question_13_13/Question_13_13.java
@@ -0,0 +1,30 @@
+package week_13.Question_13_13;
+
+public class Question_13_13 {
+    public static void main(String[] args) throws CloneNotSupportedException {
+        Course course1 = new Course("Inar Academy");
+        System.out.println("Course Name: " + "\"" + course1.toString() + "\"");
+
+        course1.addStudents("Burak");
+        course1.addStudents("Emre");
+        course1.addStudents("Furkan");
+        course1.addStudents("Jack");
+
+        Course course2 = course1.clone();
+        course2.addStudents("Jonny");
+        course2.addStudents("Tonny");
+
+        System.out.println("\nNumber of students in course1 :" + course1.getNumberOfStudents());
+        String[] students1 = course1.getStudents();
+        for (int i = 0; i < course1.getNumberOfStudents(); i++) {
+            System.out.print(students1[i] + ", ");
+        }
+        System.out.println("\n---------------------------------------");
+
+        System.out.println("Number of students in course1 :" + course2.getNumberOfStudents());
+        String[] students2 = course2.getStudents();
+        for (int i = 0; i < course2.getNumberOfStudents(); i++) {
+            System.out.print(students2[i] + ", ");
+        }
+    }
+}
